5ec9fbe49f94a688c2637fb23759a97450f39a60
nakataku119 <t.nakahara.1109@icloud.com>
2023-03-10 00:01:33 +0900

    axiosのinterseptorsの設定

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,27 +1,34 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import axios from "axios";
+import axios, { AxiosRequestConfig, AxiosInstance } from "axios";
 import { NextPage } from "next";
 import React, { useEffect } from "react";
 
+export const axiosClient: AxiosInstance = axios.create({
+  baseURL: "http://localhost:3333",
+  headers: { "Content-Type": "application/json" },
+  timeout: 5000,
+});
+
 const HomePage: NextPage = () => {
   const { isAuthenticated, getAccessTokenSilently, logout } = useAuth0();
-
+  // Auth0の認証後にAxiosのリクエストヘッダーにtokenを設定
   useEffect(() => {
-    const getToken = async () => {
+    const setTokenToAxios = async () => {
       const accessToken = await getAccessTokenSilently({});
-      console.log(accessToken);
+      // confitのタイプ要確認
+      axiosClient.interceptors.request.use((config: any) => {
+        config.headers = {
+          Authorization: "Bearer " + accessToken,
+        };
+        return config;
+      });
     };
-    getToken();
+    setTokenToAxios();
   }, []);
 
   const checkAPI = async () => {
     console.log("click");
-    const accessToken = await getAccessTokenSilently({});
-    axios.get("http://localhost:3333/users", {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+    axiosClient.get("/users");
   };
 
   return (
