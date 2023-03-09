a656db3f4f86bebece82484d3996142bbc251412
nakataku119 <t.nakahara.1109@icloud.com>
2023-03-09 21:56:04 +0900

    ログイン後にアクセストークンを取得しログに表示

--- pages/home.tsx
+++ pages/home.tsx
@@ -5,6 +5,14 @@ import React, { useEffect } from "react";
 const HomePage: NextPage = () => {
   const { isAuthenticated, getAccessTokenSilently, logout } = useAuth0();
 
+  useEffect(() => {
+    const getToken = async () => {
+      const accessToken = await getAccessTokenSilently({});
+      console.log(accessToken);
+    };
+    getToken();
+  }, []);
+
   return (
     <div>
       <h2>ログイン状態</h2>
