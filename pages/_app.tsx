ef177b0906d91ecbdc0e37126f490a250d302ed3
nakataku119 <t.nakahara.1109@icloud.com>
2023-03-14 16:30:36 +0900

    共通レイアウトの調整

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import { AxiosClientProvider } from "@/axios/AxiosClientProvider";
 import AppHeader from "@/components/organisms/AppHeader";
 import AppFooter from "@/components/organisms/AppFooter";
+import styles from "@/styles/Home.module.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const redirectUri = `${process.env.NEXT_PUBLIC_BASE_URL}/home`;
@@ -19,7 +20,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <AxiosClientProvider>
         <>
           <AppHeader />
-          <Component {...pageProps} />
+          <main className={styles.main}>
+            <Component {...pageProps} />
+          </main>
           <AppFooter />
         </>
       </AxiosClientProvider>
