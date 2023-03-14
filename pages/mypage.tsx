682f678a9caff18cf9f6ef923675ba42bb06be0b
nakataku119 <t.nakahara.1109@icloud.com>
2023-03-14 19:35:08 +0900

    新規ミーティング作成ボタンを追加

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,20 +1,20 @@
 import MeetingCardContainer from "@/components/organisms/MeetingCardContainer";
 import MemberCardContainer from "@/components/organisms/MemberCardContainer";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { NextPage } from "next";
 import React from "react";
 
 const MyPage: NextPage = () => {
   return (
     <Box sx={{ width: 1, height: "100vh", border: 1 }}>
-      <Typography
-        variant="h5"
-        component="h1"
-        color="text.secondary"
-        sx={{ pb: 1 }}
-      >
-        今後のミーティング
-      </Typography>
+      <Box sx={{ display: "flex", p: 1, justifyContent: "space-between" }}>
+        <Typography variant="h5" component="h1" color="text.secondary">
+          今後のミーティング
+        </Typography>
+        <Button size="small" variant="outlined">
+          新規ミーティングを設定
+        </Button>
+      </Box>
       <MeetingCardContainer />
       <Typography
         variant="h5"
