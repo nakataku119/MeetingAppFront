a129411abf22d8521d0437323c432317642428a5
nakataku119 <t.nakahara.1109@icloud.com>
2023-03-14 14:37:17 +0900

    フッターの作成

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { AxiosClientProvider } from "@/axios/AxiosClientProvider";
 import AppHeader from "@/components/organisms/AppHeader";
+import AppFooter from "@/components/organisms/AppFooter";
 
 export default function App({ Component, pageProps }: AppProps) {
   const redirectUri = `${process.env.NEXT_PUBLIC_BASE_URL}/home`;
@@ -19,6 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <>
           <AppHeader />
           <Component {...pageProps} />
+          <AppFooter />
         </>
       </AxiosClientProvider>
     </Auth0Provider>
