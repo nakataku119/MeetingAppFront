2e0c899f88265d1c0cd2a02755d59825701031ff
nakataku119 <t.nakahara.1109@icloud.com>
2023-03-14 19:29:39 +0900

    要素がはみ出す時に折り返すよう修正

--- components/organisms/MemberCardContainer.tsx
+++ components/organisms/MemberCardContainer.tsx
@@ -3,7 +3,10 @@ import MemberCard from "./MemberCard";
 
 export default function MemberCardContainer() {
   return (
-    <Box sx={{ height: "50%", border: 1, display: "flex" }}>
+    <Box sx={{ height: "50%", border: 1, display: "flex", flexWrap: "wrap" }}>
+      <MemberCard />
+      <MemberCard />
+      <MemberCard />
       <MemberCard />
       <MemberCard />
       <MemberCard />
