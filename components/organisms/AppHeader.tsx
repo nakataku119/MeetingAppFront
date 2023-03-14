2c5120f5550c94110127f1174dd0fafe795b3242
nakataku119 <t.nakahara.1109@icloud.com>
2023-03-14 14:18:26 +0900

    Logoをクリックでルートに戻る、ログインボタンの実装

--- components/organisms/AppHeader.tsx
+++ components/organisms/AppHeader.tsx
@@ -4,10 +4,11 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import Link from "next/link";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function AppHeader() {
+  const { loginWithRedirect } = useAuth0();
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
@@ -16,9 +17,11 @@ export default function AppHeader() {
       >
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            LOGO
+            <Link href="/">LOGO</Link>
           </Typography>
-          <Button color="inherit">ログイン</Button>
+          <Button color="inherit" onClick={() => loginWithRedirect()}>
+            ログイン
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
