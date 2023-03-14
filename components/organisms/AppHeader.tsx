3923c67d05b0dcafc62e7fe4dcb1476e06f37321
nakataku119 <t.nakahara.1109@icloud.com>
2023-03-14 14:44:23 +0900

    ログインボタンとログアウトボタンの切り替え

--- components/organisms/AppHeader.tsx
+++ components/organisms/AppHeader.tsx
@@ -8,20 +8,23 @@ import Link from "next/link";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function AppHeader() {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar
-        position="static"
-        sx={{ color: "#cff09e", backgroundColor: "#3b8686" }}
-      >
+      <AppBar sx={{ color: "#cff09e", backgroundColor: "#3b8686" }}>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             <Link href="/">LOGO</Link>
           </Typography>
-          <Button color="inherit" onClick={() => loginWithRedirect()}>
-            ログイン
-          </Button>
+          {isAuthenticated ? (
+            <Button color="inherit" onClick={() => logout()}>
+              ログアウト
+            </Button>
+          ) : (
+            <Button color="inherit" onClick={() => loginWithRedirect()}>
+              ログイン
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
