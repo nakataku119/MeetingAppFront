a32cefaaead3a9c124e9415f4313a230cbfe6571
nakataku119 <t.nakahara.1109@icloud.com>
2023-03-14 19:00:46 +0900

    mypageのレイアウト調整

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,15 +1,28 @@
-import MeetingCard from "@/components/organisms/MeetingCard";
-import { useAuth0 } from "@auth0/auth0-react";
-import { Box, Container } from "@mui/material";
+import MeetingCardContainer from "@/components/organisms/MeetingCardContainer";
+import { Box, Typography } from "@mui/material";
 import { NextPage } from "next";
 import React from "react";
 
 const MyPage: NextPage = () => {
-  const { isAuthenticated, getAccessTokenSilently, logout } = useAuth0();
   return (
     <Box sx={{ width: 1, height: "100vh", border: 1 }}>
-      <p>mypage</p>
-      <MeetingCard />
+      <Typography
+        variant="h5"
+        component="h1"
+        color="text.secondary"
+        sx={{ pb: 1 }}
+      >
+        今後のミーティング
+      </Typography>
+      <MeetingCardContainer />
+      <Typography
+        variant="h5"
+        component="h1"
+        color="text.secondary"
+        sx={{ pb: 1, pt: 3 }}
+      >
+        チームメンバー
+      </Typography>
     </Box>
   );
 };
