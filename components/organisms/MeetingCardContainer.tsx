991113b33ff2250117049406095fb625fd3c342d
nakataku119 <t.nakahara.1109@icloud.com>
2023-03-15 17:36:06 +0900

    Meetingがない時の画面切り替え

--- components/organisms/MeetingCardContainer.tsx
+++ components/organisms/MeetingCardContainer.tsx
@@ -4,11 +4,19 @@ import MeetingCard from "./MeetingCard";
 
 export default function MeetingCardContainer(props: { joinedMtgs: Mtg[] }) {
   const { joinedMtgs } = props;
-  return (
-    <Box sx={{ height: "40%", display: "flex" }}>
-      {joinedMtgs.map((item: Mtg, index: number) => (
-        <MeetingCard meeting={item} key={index} />
-      ))}
-    </Box>
-  );
+  if (joinedMtgs.length > 0) {
+    return (
+      <Box sx={{ height: "40%", display: "flex" }}>
+        {joinedMtgs.map((item: Mtg, index: number) => (
+          <MeetingCard meeting={item} key={index} />
+        ))}
+      </Box>
+    );
+  } else {
+    return (
+      <Box sx={{ height: "40%", display: "flex" }}>
+        ミーティングの予定はありません。
+      </Box>
+    );
+  }
 }
