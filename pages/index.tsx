db1054cd943b656c84bb73390b6083bb3f90b18f
nakataku119 <t.nakahara.1109@icloud.com>
2023-03-31 17:51:25 +0900

    ログイン済みの場合、トップからマイページへ遷移

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import LoginOrSingupButton from "@/components/organisms/LoginOrSignupButton";
+import { CurrentUserContext } from "@/contexts/CurrentUserProvider";
 import { Link } from "@mui/material";
+import { useRouter } from "next/router";
+import { useContext } from "react";
 
 export default function Home() {
+  const { currentUser } = useContext(CurrentUserContext);
+  const router = useRouter();
+
+  if (currentUser) {
+    router.push("/mypage");
+    return null;
+  }
+
   return (
     <>
       <Link href="/guest" sx={{ mb: 3 }}>
